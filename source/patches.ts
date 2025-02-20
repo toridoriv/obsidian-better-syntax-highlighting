1134916a26797ef7e8065f0dd8b6c155cea0597b
Victoria Rodriguez <vrodriguezfe@icloud.com>
2025-02-19 22:29:26 -0300

    ✨ Add Prism.js support and enhance code block styling

--- source/patches.ts
+++ source/patches.ts
@@ -1,16 +1,36 @@
+import { loadPrism } from "obsidian";
+import type { Grammar, GrammarRest, GrammarValue, TokenObject } from "prismjs";
+
+type PrismGrammar = GrammarRest & Record<string, GrammarValue>;
+
+function getGrammarToken(grammar: PrismGrammar, key: string): TokenObject {
+  const def = grammar[key];
+  const fallback = { pattern: /^\b$/ };
+
+  if (!def) return fallback;
+  if (def instanceof RegExp) return { pattern: def };
+  if (Array.isArray(def)) return fallback;
+
+  return def;
+}
+
 export function patchCodeMirror() {
   const token = "if (stream != state.thisLine.stream)";
   const { CodeMirror } = window;
-  const markdownMode = CodeMirror.modes.markdown.toString();
-  const index = markdownMode.indexOf(token);
-  const firstHalf = markdownMode
+  const markdownMode = CodeMirror.modes.markdown;
+
+  if (markdownMode.isPatch) return;
+
+  const markdownModeBody = markdownMode.toString();
+  const index = markdownModeBody.indexOf(token);
+  const firstHalf = markdownModeBody
     .substring(0, index)
     .replace("function(cmCfg, modeCfg)", "");
-  const secondHalf = markdownMode.substring(index);
+  const secondHalf = markdownModeBody.substring(index);
   const fix = "state.quote = 0;";
-  const corrected = [firstHalf, fix, secondHalf].join("");
-
-  if (markdownMode.includes(fix + token)) return;
+  const corrected = [firstHalf, fix, secondHalf]
+    .join("")
+    .replace("modeCfg.strikethrough = false;", "modeCfg.strikethrough = true;");
 
   const createMdFactory = new Function(
     "cmCfg",
@@ -25,7 +45,59 @@ export function patchCodeMirror() {
     value: ["xml"],
   });
 
+  Object.defineProperty(markdownModeFactory, "isPatch", {
+    enumerable: false,
+    configurable: false,
+    value: true,
+  });
+
   CodeMirror.defineMode("markdown", markdownModeFactory);
+}
+
+export async function patchPrism() {
+  const Prism = await loadPrism();
+  const markdown = Prism.languages.markdown as PrismGrammar;
+  const bold = {
+    ...getGrammarToken(markdown, "bold"),
+    pattern: /\*{2}[^\n\r]+\*{2}|_{2}[^\n\r]+_{2}/gm,
+    alias: ["inline"],
+  };
+  const italic = {
+    ...getGrammarToken(markdown, "italic"),
+    pattern:
+      /\*{1}[^\n\r *]{1,}[^\n\r*]+[^\n\r *]{1,}\*{1}(?!.*\*)|_{1}[^\n\r _]{1,}[^\n\r_]+[^\n\r _]{1,}_{1}(?!.*_)/gm,
+    alias: ["inline"],
+  };
+  const grammar: PrismGrammar = {
+    blockquote: {
+      alias: ["block", "thematic-break"],
+      pattern: /^>{1,} ?[^\n\r]*[\n|\r]{0,}/gm,
+      inside: {
+        bold,
+        italic,
+      },
+    },
+    bold,
+    italic,
+    list: {
+      ...getGrammarToken(markdown, "list"),
+      alias: ["block", "thematic-break"],
+      pattern: /^[ \t]{0,}(\d{1,}\. [^\n\r]*[\n|\r]{0,1}|[-*+] [^\n\r]*[\n|\r]{0,1})/gm,
+      greedy: false,
+      lookbehind: false,
+      inside: {
+        bold,
+        italic,
+      },
+    },
+  };
+
+  Prism.languages["markdown"] = Prism.languages.extend("markdown", grammar);
+  Prism.languages["md"] = Prism.languages.extend("md", grammar);
+
+  if (process.env.NODE_ENV === "development") {
+    Prism.highlightAll();
+  }
 }
 
 declare global {
@@ -34,4 +106,17 @@ declare global {
   }
 
   type CodeMirror = typeof import("codemirror");
+
+  namespace CodeMirror {
+    interface ModeFactory<T> {
+      dependencies: string[];
+      isPatch: boolean;
+    }
+  }
+}
+
+declare module "obsidian" {
+  type Prism = typeof import("prismjs");
+
+  export function loadPrism(): Promise<Prism>;
 }
--- source/plugin.ts
+++ source/plugin.ts
@@ -1,11 +1,19 @@
 import { Plugin } from "obsidian";
 import { Announcer } from "./announcer.ts";
-import { patchCodeMirror } from "./patches.ts";
+import { patchCodeMirror, patchPrism } from "./patches.ts";
 
 export class BetterCodeBlocksPlugin extends Plugin {
   onload(): Promise<void> | void {
     Announcer.initialize(this);
     patchCodeMirror();
+
+    patchPrism()
+      .then(() => {
+        Announcer.instance.info("Prism.js loaded!", 3_000);
+      })
+      .catch((err) => {
+        Announcer.instance.error(err.message, err, 3_000);
+      });
 
     Announcer.instance.info("Better Code Blocks plugin loaded!", 3_000);
   }
